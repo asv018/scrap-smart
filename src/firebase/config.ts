c11c5c780bc8d0029797334e7323832b385e0432
Arpit Singh Verma <21bcs089@nith.ac.in>
2024-03-10 01:36:47 +0530

    config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -24,3 +24,23 @@ const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 const storage = getStorage(app);
 export { auth, db, provider, storage };
+
+// 2nd method
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCbHeYhGZbo7gf6vpw3KchAtNKbFrxe7pw",
+//   authDomain: "scrap-smart.firebaseapp.com",
+//   projectId: "scrap-smart",
+//   storageBucket: "scrap-smart.appspot.com",
+//   messagingSenderId: "976881160326",
+//   appId: "1:976881160326:web:2f6d2696534096933f0107"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
